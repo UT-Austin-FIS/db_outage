1bee55e1dae14fc190603cd1cf64a200f209b8da
Todd Graves <tgraves@austin.utexas.edu>
2014-05-16 10:59:19 -0500

    making outage_url configurable

--- db_outage/urls.py
+++ db_outage/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from db_outage.views import DBOutage
 
+try:
+    outage_url = settings.OUTAGE_URL
+except AttributeError:
+    outage_url = r'^outage/$'
+
 urlpatterns = patterns('',
-    url(r'^outage/$', DBOutage.as_view(), name='db_outage',),
+    url(outage_url, DBOutage.as_view(), name='db_outage',),
 )
