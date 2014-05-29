862fcd32f777e0e7f4bb450fd8c0d1c583f2b021
Todd Graves <tgraves@austin.utexas.edu>
2014-05-29 10:03:49 -0500

    simplifying context object logic

--- db_outage/views.py
+++ db_outage/views.py
@@ -1,17 +1,16 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.shortcuts import redirect
 from django.views.generic.base import TemplateView
-
-from utdirect.templates import UTDirectContext, UTDirectTemplateAPIError
 
 try:
     module_path, ctx_class = settings.DB_OUTAGE_CONTEXT.rsplit('.',1)
     module = __import__(module_path, fromlist=[ctx_class])
     context = getattr(module, ctx_class)
 except AttributeError:
-    context = UTDirectContext
-
+    raise ImproperlyConfigured(
+        'You must supply a path your own context object by setting a '
+        'DB_OUTAGE_CONTEXT in your settings.py file.'
+    )
 
 class DBOutage(TemplateView):
     template_name = 'db_outage/db_outage.html'
@@ -27,25 +26,10 @@ class DBOutage(TemplateView):
                 'been notified.'
             )
         ctx.update({'msg':msg})
-        try:
-            new_context = context(self.request, ctx)
-        except UTDirectTemplateAPIError:
-            try:
-                api_key = settings.API_KEY
-            except AttributeError:
-                raise ImproperlyConfigured(
-                    'If you do not supply your own context object by setting '
-                    'a DB_OUTAGE_CONTEXT in settings.py, then you must supply '
-                    'an API_KEY in your settings, which will be used to call '
-                    'the default UTDirectContext.'
-                )
-
-            new_context = context(
-                self.request,
-                dict=ctx,
-                api_key=api_key,
-                page_title='Service Outage',
-                window_title='Service Outage',
+        return context(
+            self.request,
+            ctx,
+            title='Service Outage',
+            page_title='Service Outage',
+            window_title='Service Outage',
             )
-
-        return new_context
